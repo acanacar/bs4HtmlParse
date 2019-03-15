5a25a2c4acebf19460f51e60cfdc45260dfb5f50
acanacar <ahmetcanacar.1@gmail.com>
2019-03-15 11:57:14 +0300

    renaming py is changed

--- renaming.py
+++ renaming.py
@@ -12,11 +12,14 @@ def zipFile(path_to_zip_file, directory_to_extract_to):
 
 def rename(dir, pattern, titlePattern):
     for pathAndFilename in glob.iglob(os.path.join(dir, pattern)):
-        print(pathAndFilename)
         title, ext = os.path.splitext(os.path.basename(pathAndFilename))
         title = title[:-14] + title[-7:]
-        print('ext: ', ext)
         os.rename(pathAndFilename,
                   os.path.join(dir, titlePattern % title))
 
+def rename2016Before(dir, pattern, titlePattern):
+    for pathAndFilename in glob.iglob(os.path.join(dir, pattern)):
+        title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+        os.rename(pathAndFilename,
+                  os.path.join(dir, titlePattern % title))
 
