0f19127840d0f3d33a5a7222223c3b4bddca9134
Ahmet Can Acar <ahmetcanacar.1@gmail.com>
2019-03-29 21:41:04 +0300

    Bokeh library sample project which is going to be benefit from is added

--- secim/v3.py
+++ secim/v3.py
@@ -16,8 +16,30 @@ dfLib = readTuikData(path=path)
 pathGsyh = '/home/cem/PycharmProjects/htmlParseInf/secim/gsyh.csv'
 dfGsyh = readTuikData(path=pathGsyh)
 
-
-
 df = dfSelection.join(dfLib, on='city')
 df = df.set_index(['plaka', 'city'])
+
+
+from bokeh.plotting import figure
+from bokeh.io import show, output_notebook
+
+# Create a blank figure with labels
+p = figure(plot_width = 600, plot_height = 600, 
+           title = 'Example Glyphs',
+           x_axis_label = 'X', y_axis_label = 'Y')
+
+# Example data
+squares_x = [1, 3, 4, 5, 8]
+squares_y = [8, 7, 3, 1, 10]
+circles_x = [9, 12, 4, 3, 15]
+circles_y = [8, 4, 11, 6, 10]
+
+# Add squares glyph
+p.square(squares_x, squares_y, size = 12, color = 'navy', alpha = 0.6)
+# Add circle glyph
+p.circle(circles_x, circles_y, size = 12, color = 'red')
+
+
+
+
 
