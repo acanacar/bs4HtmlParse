389c5dbd3f0eb74dd7ac95d3dfcbefad64bdf085
acanacar <ahmetcanacar.1@gmail.com>
2019-04-18 02:07:58 +0300

    subTotalControl is modified

--- subTotalControl.py
+++ subTotalControl.py
@@ -2,36 +2,46 @@ import pandas as pd
 from BilancoParse.constants import *
 
 stocks = Bist30Stocks
-period = '2018_2'
-periods = ['2018_1', '2018_2', '2018_3']
+periods = [
+    '2016_1', '2016_2', '2016_3', '2016_4',
+    '2017_1', '2017_2', '2017_3', '2017_4',
+    '2018_1', '2018_2', '2018_3', '2018_4'
+]
+
+
+def getMistakes(data):
+    SubCodeUnique = set(data['LastSubCode'].values)
+    mistakeSubCodes = []
+    for i in SubCodeUnique:
+        if type(i) == str:
+            j = int(i[:-1])
+            subTotal = sum(data.loc[data['LastSubCode'] == i, 'colDipToplam'])
+            checkTotal = data.loc[data['tableindex'] == j, 'colDipToplam'].values
+            if checkTotal[0] - subTotal != 0:
+                print(i)
+                print(checkTotal)
+                print(subTotal)
+                mistakeSubCodes.append(i)
+            else:
+                pass
+                print('{} => Dogru, checkTotal => {} subTotal=> {}'.format(i, checkTotal, subTotal))
+    return mistakeSubCodes
+
+
+years = [2016, 2017, 2018]
+
+for year in years:
 
 d = []
 for stock in stocks:
     for period in periods:
         try:
-            path = '/home/cem/Desktop/diptoplamChecks/{}_{}_PartB.pkl'.format(stock, period)
-
+            path = '/home/cem/Desktop/diptoplamChecks/pickles/{}_{}_PartB.pkl'.format(stock, period)
             df = pd.read_pickle(path)
         except Exception as e:
             print('{} {} bulunamadi.'.format(stock, period))
             continue
-        SubCodeUnique = set(df['LastSubCode'].values)
-
-        mistakeSubCodes = []
-        for i in SubCodeUnique:
-            if type(i) == str:
-                j = int(i[:-1])
-                subTotal = sum(df.loc[df['LastSubCode'] == i, 'colDipToplam'])
-                checkTotal = df.loc[df['tableindex'] == j, 'colDipToplam'].values
-
-                if checkTotal[0] - subTotal != 0:
-                    print(i)
-                    print(checkTotal)
-                    print(subTotal)
-                    mistakeSubCodes.append(i)
-                else:
-                    print('{} => Dogru, checkTotal => {} subTotal=> {}'.format(i, checkTotal, subTotal))
-
+        mistakeSubCodes = getMistakes(data=df)
         if len(mistakeSubCodes) > 0:
             d.append({'period': period, 'stock': stock, 'mistakes': mistakeSubCodes})
             print('{} yanlis calculation bulundu', stock)
